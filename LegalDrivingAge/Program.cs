3877bfddc016d2159052ada6f263abad5b5c3b67
Ben Stoica <bennystoica@gmail.com>
2020-05-27 19:44:20 -0400

    Add files via upload

---
+++ LegalDrivingAge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LegalDrivingAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Project: Legal Driving Age");
+            Console.WriteLine("We are going to tell you if it is legal for you to drive!");
+            Console.WriteLine("What is your age?");
+            int age = Int32.Parse(Console.ReadLine());
+
+            if (age < 0) {
+                Console.WriteLine("You have entered an invalid age, please enter a valid age.");
+                age = Int32.Parse(Console.ReadLine());
+            }
+
+            var result = age >= 16 ? "You are old enough to legally drive!" : "You are not old enough to legally drive!";
+            Console.WriteLine(result);
+        }
+    }
+}
